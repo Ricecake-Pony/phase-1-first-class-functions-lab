c2e01643f4f0c8648b2cc67dfb793df9107e9537
Trey <trey.delcamp@gmail.com>
2022-09-23 10:57:56 -0500

    commit tree db26848c04fbc707d0be3d89b26ae2be6f9d219b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers =( drivers) => {
+    return drivers.slice( 0, 2)
+
+}
+
+const returnLastTwoDrivers = ( drivers) => {
+    return drivers.slice(drivers.length - 2)
+
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier =( number )=> 
+    function higherOrderDumbDumb(fare) {
+        return fare * number
+    }
+
+    const fareDoubler =( fare )=> createFareMultiplier( 2 )(fare);
+
+    const fareTripler =( fare )=> createFareMultiplier( 3 )(fare);
+
+    function selectDifferentDrivers( drivers, selectDrivers) {
+        return selectDrivers( drivers )
+    }
